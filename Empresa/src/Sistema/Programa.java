8be69201c35bc9cac7b659f0fb8fa0d1fae77d2a
Arthur Phillip Silva <artphil7@yahoo.com.br>
2015-12-01 17:50:20 -0200

    Criada a interface de entrada

--- Empresa/src/Sistema/Programa.java
+++ Empresa/src/Sistema/Programa.java
@@ -1,15 +1,18 @@
 package Sistema;
 
+import javax.swing.*;
+
 import Interface.*;
 import Pessoas.*;
 import Servicos.*;
 
 public class Programa {
+	
+	private static JFrame janela;
 
-	public static void main(String[] args) {
-
-		
-		//Janela principal = new Janela();
+	public static void main(String[] args) 
+	{
+		janela = new JanelaInicio();
 	}
 
 }
